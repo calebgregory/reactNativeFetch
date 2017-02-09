2f7e380065a554c424db0189b3b044a0900801b9
Caleb Gregory <calebgregory@github.com>
2017-02-09 17:07:50 -0600

    Adding protobuf.js, starting rudimentary impl

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+const protobuf = require('protobufjs');
+
+let MsgReply;
+let MsgRequest;
+protobuf.load("../proto/data.proto", (err, root) => {
+  if (err) throw err;
+
+  MsgRequest = root.lookup("MsgRequest");
+  MsgReply   = root.lookup("MsgReply");
+});
+
 app.use('/api/binary', (req, res) => {
-  const data = [0x4D, 0x41, 0x20, 0x44, 0x41, 0x57, 0x47];
-  res.send(Buffer.from(data));
+  // TODO: marshal Request, send reply
+  res.send(1337);
 });
 
 app.listen('3030', (err) => {
