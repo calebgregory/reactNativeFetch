064284f801637652d0593d4e6154e270d86b8e04
Caleb Gregory <calebgregory@github.com>
2017-02-10 12:39:43 -0600

    Moving around server-side code

--- server/index.js
+++ server/index.js
@@ -1,27 +1,97 @@
+const path = require('path');
 const express = require('express');
-const app = express();
-
 const protobuf = require('protobufjs');
 
-let MsgReply;
-let MsgRequest;
-protobuf.load("../proto/data.proto", (err, root) => {
+const app = express();
+
+/* Load protocol buffer root */
+let root = false;
+protobuf.load(path.resolve(__dirname, './data.proto'), (err, _root) => {
   if (err) throw err;
 
-  MsgRequest = root.lookup("MsgRequest");
-  MsgReply   = root.lookup("MsgReply");
+  root = _root;
 });
 
-app.use('/api/binary', (req, res) => {
-  // TODO: marshal Request, send reply
-  res.send(1337);
-});
+function init(root, app, protobufs) {
+  const server = getServer(root, protobufs)
+  console.log(server);
 
-app.listen('3030', (err) => {
-  if (err) {
-    console.log('Error serving !', err);
+  const handle = getHandler(server)
+
+  app.use('/api', handle);
+
+  app.listen('3030', (err) => {
+    if (err) {
+      console.log('Error serving !', err);
+      return;
+    }
+
+    console.log('Server listening on http://localhost:3030');
+  });
+}
+
+function getServer(root /* protobuf */, protobufs /* [protobuf_name, [[service_name, service_methods]]] */) {
+  const server = protobufs
+    .reduce((acc, [protobufName, [serviceName, methods] ]) => {
+      const servicePath = `${protobufName}.${serviceName}`;
+      const Service = root.lookup(servicePath);
+      const service = {
+        [`/${servicePath}/`]: Service.create(methods)
+      };
+
+      return Object.assign( {}, acc, service );
+    }, {});
+
+  return server;
+}
+
+function getHandler(server) {
+  return function handle(req, res) {
+    const service = req.headers['x-rpc-service'];
+    const method  = req.headers['x-rpc-method'];
+
+    try {
+      const ctx = {};
+      server[service][method](ctx, req.body)
+        .then((resp) => {
+          res.send(resp);
+        })
+        .catch((error) => {
+          console.error(`[prom-caught] :( calling service func. service: ${service} method: ${method} error: ${error}`)
+          res.sendStatus(500);
+        })
+    } catch(error) {
+      console.error(`[try-caught] :( calling service func. service: ${service} method: ${method} error: ${error}`)
+      res.sendStatus(500);
+    }
+  }
+}
+
+/* Service Implementations, formated [service_name, { methods }] */
+const messagerService = [ 'Messager', {
+  sendMsg: (ctx, request, cb) => {
+    // just send back a simple response 'ok'.
+    return cb(null, { caller: 'server', response: 'ok', request });
+  }
+}];
+
+/* array of tuples [protobuf_name, service_tuples] */
+const protobufs = [
+  ['data', messagerService]
+];
+
+function tryInit([t0, t1], n) {
+  if (n > 10) {
+    console.log("Tried 10 times and still no root. Sorry.")
     return;
   }
 
-  console.log('Server listening on http://localhost:3030');
-});
+  if (root) {
+    init(root, app, protobufs);
+    return;
+  }
+
+  setTimeout(() => tryInit([t1, t0 + t1], n + 1), t1)
+}
+
+tryInit([0, 100], 0)
